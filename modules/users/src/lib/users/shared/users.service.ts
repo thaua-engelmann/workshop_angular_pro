b597422aa790a9372f408b8f1936c85b304431e7
thaua-engelmann <engelmann.webdev@gmail.com>
2024-05-18 19:11:58 -0300

    feat(lib-users): created both getUsers and getUserById http methods

--- modules/users/src/lib/users/shared/users.service.ts
+++ modules/users/src/lib/users/shared/users.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { ENV_DEV } from './../../../../../../src/environments/environment';
 import { Injectable } from '@angular/core';
+import { User } from '../types/user';
+
+const API_KEY = ENV_DEV.API_KEY;
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUsers = () => {
+    return this.http.get<User[]>(`${API_KEY}`);
+  }
+
+  getUserById = (id: string) => {
+    return this.http.get<User>(`${API_KEY}/${id}`);
+  }
 }
